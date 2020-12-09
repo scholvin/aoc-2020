c5f965b49cbf9dc879d06be5d870768f78b45c5e
John Scholvin <john@scholvin.com>
2020-12-09 08:46:48 -0600

    day 9

--- src/week2.cpp
+++ src/week2.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 #include <set>
 #include <fstream>
+#include <iostream>
 
 namespace week2 
 {
@@ -102,6 +103,87 @@ namespace week2
                     return accumulator;
             }
         }
+        // shouldn't happen
+        return -1;
+    }
+
+    const size_t PREAMBLE = 25;
+
+    long day09a()
+    {
+        std::ifstream infile("../data/day09.dat");
+        std::string line;
+        std::deque<long> queue;
+
+        // seed
+        for (size_t i = 0; i < PREAMBLE; ++i)
+        {
+            std::getline(infile, line);
+            queue.push_back(std::stol(line));
+        }
+
+        while (std::getline(infile, line))
+        {
+            long next = std::stol(line);
+            bool found = false;
+            // O(N^2), don't care
+            for (size_t i = 0; i < PREAMBLE; ++i)
+                for (size_t j = 1; j < PREAMBLE; ++j)
+                    if (queue[i] + queue[j] == next)
+                    {
+                        found = true;
+                        break;
+                    }
+            if (!found)
+                return next;
+            queue.pop_front();
+            queue.push_back(next);
+        }
+
+        // shouldn't happen
+        return -1;
+    }
+
+    long day09b()
+    {
+        std::ifstream infile("../data/day09.dat");
+        std::string line;
+        std::vector<long> queue;
+
+        while (std::getline(infile, line))
+        {
+            long next = std::stol(line);
+            queue.push_back(next);
+        }
+
+        const long invalid_sum = day09a();  // ~25ms
+
+        // brute force, another O(N^2), surely there's an O(N) way to do this by wiggling the ends of a range
+        for (size_t i = 0; i < queue.size(); ++i)
+        {
+            long working_sum = queue[i];
+            for (size_t j = i+1; j < queue.size(); ++j)
+            {
+                working_sum += queue[j];
+                if (working_sum > invalid_sum)
+                {
+                    break;
+                }
+                if (working_sum == invalid_sum)
+                {
+                    // done, now find smallest and largest, which are NOT first and last
+                    long min = std::numeric_limits<long>::max();
+                    long max = 0;
+                    for (size_t k = i; k <= j; k++)
+                    {
+                        if (queue[k] > max) max = queue[k];
+                        if (queue[k] < min) min = queue[k];
+                    }
+                    return min + max;
+                }
+            }
+        }
+
         // shouldn't happen
         return -1;
     }
--- src/week2.h
+++ src/week2.h
@@ -6,10 +6,14 @@ namespace week2
 {
     long day08a();
     long day08b();
+    long day09a();
+    long day09b();
 
 	const runner::method_map_t method_map =
 	{
         { "8a", &day08a },
         { "8b", &day08b },
+        { "9a", &day09a },
+        { "9b", &day09b },
     };
 };
