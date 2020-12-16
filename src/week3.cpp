bc811dc05951ac4d39a4fe1a412fd7354601efdc
John Scholvin <john@scholvin.com>
2020-12-16 09:42:43 -0600

    day16a, brute force

--- src/week3.cpp
+++ src/week3.cpp
@@ -3,6 +3,11 @@
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
+#include <regex>
+#include <algorithm>
+#include <boost/lexical_cast.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string.hpp>
 
 namespace week3 
 {
@@ -39,5 +44,78 @@ namespace week3
             turn++;
         }
         return last;
+    }
+
+    struct range
+    {
+        std::string tag;
+        long lo1, hi1, lo2, hi2;
+
+        // assumes range 1 is strictly less than range 2 (which appears to be the case in the data if not the spec)
+        bool is_valid(long f) const { return (f >= lo1 && f <= hi1) || (f >= lo2 && f <= hi2); }
+    };
+
+    typedef std::vector<long> ticket_t;
+
+    const std::regex RANGE_RE("([a-z ]+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)");
+
+    long day16a()
+    {
+        // reading this file is a pain in the arse
+        std::ifstream infile("../data/day16.dat");
+        std::string line;
+
+        std::vector<range> ranges;
+        while (true)
+        {
+            std::getline(infile, line);
+            std::smatch sm;
+            if (!std::regex_search(line, sm, RANGE_RE))
+                break;
+            ranges.push_back(range { sm.str(1), std::stol(sm.str(2)), std::stol(sm.str(3)), std::stol(sm.str(4)), std::stol(sm.str(5)) } );
+        }
+
+        std::getline(infile, line); // throw away "your ticket:"
+        std::getline(infile, line);
+        ticket_t my_ticket;
+        boost::tokenizer<> tok(line);
+        std::transform(tok.begin(), tok.end(), std::back_inserter(my_ticket), &boost::lexical_cast<long,std::string>);
+
+        std::getline(infile, line); // throw away blank line
+        std::getline(infile, line); // throw away "nearby tickets:"
+        std::vector<ticket_t> nearby_tickets;
+        while (std::getline(infile, line))
+        {
+            ticket_t next;
+            boost::tokenizer<> tok(line);
+            std::transform(tok.begin(), tok.end(), std::back_inserter(next), &boost::lexical_cast<long,std::string>);
+            nearby_tickets.push_back(next);
+        }
+
+        long sum = 0;
+        for (auto t: nearby_tickets)
+        {
+            for (auto v: t)
+            {
+                bool valid = false;
+                // the ticket values thare not valid in any range
+                for (auto r: ranges)
+                {
+                    if (r.is_valid(v))
+                    {
+                        valid = true;
+                        break;
+                    }
+                }
+                if (!valid)
+                {
+                    sum += v;
+                    break;
+                }
+
+            }
+        }
+
+        return sum;
     }
 }
--- src/week3.h
+++ src/week3.h
@@ -5,10 +5,12 @@
 namespace week3
 {
     long day15(size_t turns);
+    long day16a();
 
 	const runner::method_map_t method_map =
 	{
         { "15a", std::bind(&day15, 2020) },
         { "15b", std::bind(&day15, 30000000) },
+        { "16a", &day16a },
     };
 };
